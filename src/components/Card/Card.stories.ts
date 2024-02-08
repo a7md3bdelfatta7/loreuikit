b32fe1a672eee0b1398bae0a74b4748560af4206
Ahmed Abdelfattah <aabdelfattah@sequel.ae>
2024-02-08 20:40:04 +0200

    add title and subtitle to cards

--- src/components/Card/Card.stories.ts
+++ src/components/Card/Card.stories.ts
@@ -24,6 +24,10 @@ export const BasicCards: Story = {
   args: {
     color: Colors.Danger,
     mediaURL: "",
+    title: "Basic Card",
+    subTitle: "Sub Title",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
   },
 };
 
@@ -31,5 +35,9 @@ export const MediaCards: Story = {
   args: {
     color: Colors.Primary,
     mediaURL: "https://ionicframework.com/docs/img/demos/card-media.png",
+    title: "Media Card",
+    subTitle: "Sub Title",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
   },
 };
